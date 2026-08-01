099e55011481f17f5232391993166041977f5b6b
Adith .v.s <websinaro@gmail.com>
2026-08-01 14:19:11 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,6 @@
 from fastapi import Depends,HTTPException,status
 from fastapi.security import OAuth2PasswordBearer
-from security.security.jwt import create_access_token
+from security.jwt import create_access_token
 from sqlalchemy.orm import Session
 from datetime import datetime
 
